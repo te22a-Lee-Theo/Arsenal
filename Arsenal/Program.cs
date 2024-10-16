bfdf38df1cbf85b9ef4f4be8ee720b513ac9acb7
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-10-16 22:51:32 +0200

    deserialisering

--- Arsenal/Program.cs
+++ Arsenal/Program.cs
@@ -1,14 +1,20 @@
 ﻿global using System.Text.Json;
 
-Weapon FirstWeapon = new Weapon()
-{
-    Name = "Katana",
-    MinDmg = 5,
-    MaxDmg = 10
-};
+string qwe = File.ReadAllText("weapons.json");
 
-string jsonWeapon = JsonSerializer.Serialize(FirstWeapon);
-System.Console.WriteLine(jsonWeapon);
-File.WriteAllText("weapons.json", jsonWeapon);
+Weapon qwer = JsonSerializer.Deserialize<Weapon>(qwe);
+
+System.Console.WriteLine(qwer.MinDmg);
+
 
 Console.ReadLine();
+// Weapon FirstWeapon = new Weapon()
+// {
+//     Name = "Katana",
+//     MinDmg = 5,
+//     MaxDmg = 10
+// };
+
+// string jsonWeapon = JsonSerializer.Serialize(FirstWeapon);
+// System.Console.WriteLine(jsonWeapon);
+// File.WriteAllText("weapons.json", jsonWeapon);
